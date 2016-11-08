ac68df06febc006bd01dc5a42ceec5dd2c5fdc5c
David <zhaoweizheng66@163.com>
2016-11-08 23:04:22 +0800

    主题框架的搭建

---
+++ BuDeJieDemo/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJieDemo
+//
+//  Created by apple on 16/11/8.
+//  Copyright © 2016年 赵伟争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
